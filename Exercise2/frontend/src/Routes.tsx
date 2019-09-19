aa742fa36604d24f9e1974ddab1b3f1101335fc3
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2019-09-18 21:34:44 -0700

    feat: Use React Suspense API to lazy load components
    File sizes after gzip:
    
      89.5 KB  build/static/js/2.60cdea18.chunk.js
      3.2 KB   build/static/js/main.aff5954c.chunk.js
      799 B    build/static/js/runtime~main.44a900b7.js
    
    After:
      45.72 KB  build/static/js/4.76a075be.chunk.js
      35.33 KB  build/static/js/1.9ecab0dd.chunk.js
      6.41 KB   build/static/js/0.214c92a2.chunk.js
      5.78 KB   build/static/js/5.d006f9d4.chunk.js
      2.56 KB   build/static/js/6.9bdcbc25.chunk.js
      2.21 KB   build/static/js/7.e5abe528.chunk.js
      1.25 KB   build/static/js/runtime~main.74dd0176.js
      1.08 KB   build/static/js/8.5a7642bb.chunk.js
      719 B     build/static/js/main.5cb3a9cd.chunk.js
      307 B     build/static/js/9.4bd2580a.chunk.js

--- Exercise2/frontend/src/Routes.tsx
+++ Exercise2/frontend/src/Routes.tsx
@@ -1,23 +1,27 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Router } from "@reach/router";
-import ListNotes from "./content/ListNotes";
-import NotFound from "./content/NotFound";
-import CreateNote from "./content/CreateNote";
-import ShowNote from "./content/ShowNote";
 import PageContainer from "./components/PageContainer";
 import HomeButton from "./components/HomeButton";
+import Loading from "./components/Loading";
+
+const ListNotes = React.lazy(() => import("./content/ListNotes"));
+const CreateNote = React.lazy(() => import("./content/CreateNote"));
+const ShowNote = React.lazy(() => import("./content/ShowNote"));
+const NotFound = React.lazy(() => import("./content/NotFound"));
 
 export default () => (
-  <Router className="mt-md-4 d-flex flex-column justify-content-center">
-    <PageContainer path="/" header={<div>Your Notes</div>}>
-      <ListNotes path="/" />
-    </PageContainer>
-    <PageContainer path="/note" header={<HomeButton />}>
-      <CreateNote path="/new" />
-    </PageContainer>
-    <PageContainer path="/notes" header={<HomeButton />}>
-      <ShowNote path="/:noteId" />
-    </PageContainer>
-    <NotFound default />
-  </Router>
+  <Suspense fallback={<Loading />}>
+    <Router className="mt-md-4 d-flex flex-column justify-content-center">
+      <PageContainer path="/" header={<div>Your Notes</div>}>
+        <ListNotes path="/" />
+      </PageContainer>
+      <PageContainer path="/note" header={<HomeButton />}>
+        <CreateNote path="/new" />
+      </PageContainer>
+      <PageContainer path="/notes" header={<HomeButton />}>
+        <ShowNote path="/:noteId" />
+      </PageContainer>
+      <NotFound default />
+    </Router>
+  </Suspense>
 );
