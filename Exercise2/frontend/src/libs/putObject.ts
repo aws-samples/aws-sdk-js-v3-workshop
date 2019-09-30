297b3b30606311ad196f63c83638c67579777b20
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2019-09-30 12:55:43 -0700

    chore: import commands from root in Exercise2/frontend

--- Exercise2/frontend/src/libs/putObject.ts
+++ Exercise2/frontend/src/libs/putObject.ts
@@ -1,6 +1,6 @@
 import { s3Client } from "./s3Client";
 import { config } from "../config";
-import { PutObjectCommand } from "@aws-sdk/client-s3-browser/commands/PutObjectCommand";
+import { PutObjectCommand } from "@aws-sdk/client-s3-browser";
 
 const putObject = async (file: File) => {
   const Key = `${Date.now()}-${file.name}`;
