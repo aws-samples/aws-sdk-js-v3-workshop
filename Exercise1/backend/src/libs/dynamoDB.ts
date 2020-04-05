483ad3d7a25de2ed1bdc5232f0d4e916c4fa3a7d
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-04-05 05:55:09 -0700

    chore: remove old code

--- Exercise1/backend/src/libs/dynamoDB.ts
+++
@@ -1,3 +0,0 @@
-import AWS from "aws-sdk";
-
-export default new AWS.DynamoDB();
--- Exercise1/frontend/src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./ButtonSpinner";
-export * from "./HomeButton";
-export * from "./Loading";
-export * from "./PageContainer";
