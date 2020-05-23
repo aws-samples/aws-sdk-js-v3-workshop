983ce58a33121dcc107b0a48d51f37e9ae3e79ba
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-05-22 19:22:11 -0700

    chore: update backend from cdk branch

--- Exercise1/backend/src/getNote.ts
+++ Exercise1/backend/src/getNote.ts
@@ -1,15 +1,17 @@
 import dynamoDB from "./libs/dynamoDB";
 import { success, failure } from "./libs/response";
-import { APIGatewayProxyEvent } from "aws-lambda";
 
-export async function main(event: APIGatewayProxyEvent) {
+// eslint-disable-next-line no-unused-vars
+import { APIGatewayEvent } from "aws-lambda";
+
+export async function main(event: APIGatewayEvent) {
   const params = {
-    TableName: process.env.tableName,
+    TableName: process.env.NOTES_TABLE_NAME || "",
     // 'Key' defines the partition key and sort key of the item to be retrieved
     // - 'noteId': path parameter
     Key: {
-      noteId: { S: event.pathParameters.id }
-    }
+      noteId: { S: event?.pathParameters?.id },
+    },
   };
 
   try {
@@ -21,6 +23,7 @@ export async function main(event: APIGatewayProxyEvent) {
       return failure({ status: false, error: "Item not found." });
     }
   } catch (e) {
+    console.log(e);
     return failure({ status: false });
   }
 }
