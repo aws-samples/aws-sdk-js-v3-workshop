f98c8984d55d1b420f0d29e27f4d24e0406de591
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-05-22 21:17:24 -0700

    fix: handler was not defined for getNote

--- Exercise1/backend/src/getNote.ts
+++ Exercise1/backend/src/getNote.ts
@@ -4,7 +4,7 @@ import { success, failure } from "./libs/response";
 // eslint-disable-next-line no-unused-vars
 import { APIGatewayEvent } from "aws-lambda";
 
-export async function main(event: APIGatewayEvent) {
+export const handler = async (event: APIGatewayEvent) => {
   const params = {
     TableName: process.env.NOTES_TABLE_NAME || "",
     // 'Key' defines the partition key and sort key of the item to be retrieved
@@ -26,4 +26,4 @@ export async function main(event: APIGatewayEvent) {
     console.log(e);
     return failure({ status: false });
   }
-}
+};
