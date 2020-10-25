08bf55ffebaa200c59c79d1a4c1ba6fa7ee1da6d
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-10-25 01:00:26 -0700

    chore(deps-dev): bump webpack to 5.2.0 (#217)

--- packages/backend/webpack.config.js
+++ packages/backend/webpack.config.js
@@ -1,19 +1,23 @@
 /* eslint-disable */
 
+const { resolve } = require("path");
 const glob = require("glob");
 const ESLintPlugin = require("eslint-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-const entry = glob.sync("./src/*.ts").reduce((acc, item) => {
-  const name = item.replace("./src/", "").replace(".ts", "");
-  acc[name] = item;
-  return acc;
-}, {});
+const entry = glob.sync("./src/*.ts").reduce(
+  (acc, item) => ({
+    ...acc,
+    [item.replace("./src/", "").replace(".ts", "")]: item,
+  }),
+  {}
+);
 
 module.exports = {
   entry,
 
   output: {
+    path: resolve(__dirname, "dist/"),
     filename: "[name]/app.js",
     libraryTarget: "commonjs",
   },
