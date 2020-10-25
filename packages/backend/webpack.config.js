14b31cc1ba975c68ce5d49b7311ff166e71a26a5
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-10-25 01:00:09 -0700

    chore: update arrow function to return object

--- packages/backend/webpack.config.js
+++ packages/backend/webpack.config.js
@@ -5,11 +5,13 @@ const glob = require("glob");
 const ESLintPlugin = require("eslint-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-const entry = glob.sync("./src/*.ts").reduce((acc, item) => {
-  const name = item.replace("./src/", "").replace(".ts", "");
-  acc[name] = item;
-  return acc;
-}, {});
+const entry = glob.sync("./src/*.ts").reduce(
+  (acc, item) => ({
+    ...acc,
+    [item.replace("./src/", "").replace(".ts", "")]: item,
+  }),
+  {}
+);
 
 module.exports = {
   entry,
